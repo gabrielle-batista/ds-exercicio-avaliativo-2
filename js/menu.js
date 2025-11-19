31edf2a54f3996f08a5785a2649ebd92bbddf742
gabrielle-batista <coi.gabrielleb20@gmail.com>
2025-11-19 15:43:27 -0300

    Requisito Funcionall Cadastrar Conta

--- js/menu.js
+++ js/menu.js
@@ -10,7 +10,9 @@ $(document).ready(function(){
     
 })
 
-async function atualizarSaldo(idCliente) {
+//O menu é a camada que gerencia as operações do sistema após o login do cliente;
+
+/*async function atualizarSaldo(idCliente) {
     let saldo = 0;
 
     try {
@@ -19,4 +21,35 @@ async function atualizarSaldo(idCliente) {
         
     }
     
+}*/
+
+function gerarNumConta(nome) {
+    const letras = nome.substring(0, 2).toUpperCase();
+    const numero = Math.floor(100000 + Math.random() * 900000);
+    return `${letras}-${numero}`;
+}
+
+function cadastrarConta() {
+    let cliente = JSON.parse(localStorage.getItem('clienteAutenticado'));
+
+    // pega todas as contas do sistema
+    let contas = JSON.parse(localStorage.getItem('contas')) || [];
+
+    let numero;
+    do {
+        numero = gerarNumeroConta(cliente.nome);
+    } while (contas.some(c => c.numero === numero));
+
+    // cria conta
+    const novaConta = {
+        numero: numero,
+        saldo: 0,
+        cpf: cliente.cpf   // identifica o dono da conta
+    };
+
+    // salva no sistema
+    contas.push(novaConta);
+    localStorage.setItem('contas', JSON.stringify(contas));
+
+    alert("Conta criada: " + numero);
 }
